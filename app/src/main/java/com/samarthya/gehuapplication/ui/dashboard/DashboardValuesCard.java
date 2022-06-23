38f9b3bcc4f5f672df3c7119b6b4bbdbd2915cfc
Samarthya <samarthya2301@gmail.com>
2022-06-23 13:17:33 +0530

    values to show in a single card template

---
+++ app/src/main/java/com/samarthya/gehuapplication/ui/dashboard/DashboardValuesCard.java
@@ -0,0 +1,25 @@
+package com.samarthya.gehuapplication.ui.dashboard;
+
+public class DashboardValuesCard {
+
+	String dashboardField;
+	String dashboardKey1;
+	String dashboardKey2;
+	String dashboardValue1;
+	String dashboardValue2;
+
+	public DashboardValuesCard(String dashboardField,
+							   String dashboardKey1,
+							   String dashboardKey2,
+							   String dashboardValue1,
+							   String dashboardValue2) {
+
+		this.dashboardField = dashboardField;
+		this.dashboardKey1 = dashboardKey1;
+		this.dashboardKey2 = dashboardKey2;
+		this.dashboardValue1 = dashboardValue1;
+		this.dashboardValue2 = dashboardValue2;
+
+	}
+
+}
